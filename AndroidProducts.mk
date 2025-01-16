79174646c98ec2f1b3aaacaa83049d8ed0f08e63
F1X <96143234+f1XRK305G@users.noreply.github.com>
2025-01-16 12:16:37 +0500

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_Spacewar.mk
+    $(LOCAL_DIR)/afterlife_Spacewar.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_Spacewar-userdebug \
-    lineage_Spacewar-user \
-    lineage_Spacewar-eng
+    afterlife_Spacewar-userdebug \
+    afterlife_Spacewar-user \
+    afterlife_Spacewar-eng
